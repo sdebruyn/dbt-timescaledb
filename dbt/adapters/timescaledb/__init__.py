2386644c8cd32ab279c21b07e043633f5713a85d
Sam Debruyn <sam@debruyn.dev>
2023-11-08 00:41:19 +0100

    empty adapter

--- dbt/adapters/timescaledb/__init__.py
+++ dbt/adapters/timescaledb/__init__.py
@@ -1,11 +1,14 @@
-from dbt.adapters.timescaledb.connections import TimescaleDBConnectionManager  # noqa
-from dbt.adapters.timescaledb.connections import TimescaleDBCredentials
-from dbt.adapters.timescaledb.impl import TimescaleDBAdapter
-
 from dbt.adapters.base import AdapterPlugin
+from dbt.adapters.postgres import PostgresCredentials
+from dbt.adapters.timescaledb.connections import TimescaleDBConnectionManager, TimescaleDBCredentials
+from dbt.adapters.timescaledb.impl import TimescaleDBAdapter
 from dbt.include import timescaledb
 
-
 Plugin = AdapterPlugin(
-    adapter=TimescaleDBAdapter, credentials=TimescaleDBCredentials, include_path=timescaledb.PACKAGE_PATH
+    adapter=TimescaleDBAdapter,
+    credentials=PostgresCredentials,
+    include_path=timescaledb.PACKAGE_PATH,
+    dependencies=["postgres"],
 )
+
+__all__ = ["TimescaleDBConnectionManager", "TimescaleDBAdapter", "TimescaleDBCredentials", "Plugin"]
--- dbt/adapters/timescaledb/impl.py
+++ dbt/adapters/timescaledb/impl.py
@@ -1,20 +1,5 @@
-from dbt.adapters.sql import SQLAdapter as adapter_cls
-from dbt.adapters.timescaledb import TimescaleDBConnectionManager
+from dbt.adapters.postgres import PostgresAdapter
 
 
-class TimescaleDBAdapter(adapter_cls):
-    """
-    Controls actual implmentation of adapter, and ability to override certain methods.
-    """
-
-    ConnectionManager = TimescaleDBConnectionManager
-
-    @classmethod
-    def date_function(cls):
-        """
-        Returns canonical date func
-        """
-        return "datenow()"
-
-
-# may require more build out to make more user friendly to confer with team and community.
+class TimescaleDBAdapter(PostgresAdapter):
+    pass
