4247a2b2be99509cb3fa8e2571006859d994d811
Sam Debruyn <sam@debruyn.dev>
2023-11-08 23:47:44 +0100

    Add GitHub Actions workflow to run Pytest with TimescaleDB service and update database configuration in test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,9 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 def dbt_profile_target():
     return {
         "type": "timescaledb",
-        "host": os.getenv("POSTGRES_TEST_HOST", "localhost"),
-        "port": int(os.getenv("POSTGRES_TEST_PORT", "5432")),
-        "user": os.getenv("POSTGRES_TEST_USER", "timescaledb"),
-        "pass": os.getenv("POSTGRES_TEST_PASS", "timescaledb"),
-        "dbname": os.getenv("POSTGRES_TEST_DATABASE", "timescaledb"),
+        "host": os.getenv("TIMESCALEDB_TEST_HOST", "localhost"),
+        "port": int(os.getenv("TIMESCALEDB_TEST_PORT", "5432")),
+        "user": os.getenv("TIMESCALEDB_TEST_USER", "timescaledb"),
+        "pass": os.getenv("TIMESCALEDB_TEST_PASS", "timescaledb"),
+        "dbname": os.getenv("TIMESCALEDB_TEST_DATABASE", "timescaledb"),
     }
