8792bf980f5775c7b68ddde577a083dbfda4d8c8
Sam Debruyn <sam@debruyn.dev>
2023-11-11 20:49:55 +0100

    split up macros

--- dbt/include/timescaledb/macros/materializations/models/hypertable.sql
+++ dbt/include/timescaledb/macros/materializations/models/hypertable.sql
@@ -16,7 +16,6 @@
   {%- set preexisting_backup_relation = load_cached_relation(backup_relation) -%}
   -- grab current tables grants config for comparision later on
   {% set grant_config = config.get('grants') %}
-  {% set time_column_name = config.get('time_column_name') %}
 
   -- drop the temp relations if they exist already in the database
   {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
@@ -30,89 +29,11 @@
   -- build model
   {% call statement('main') -%}
     {{ get_create_table_as_sql(False, intermediate_relation, sql) }}
-    select create_hypertable(
-      '{{ intermediate_relation }}',
-      '{{ time_column_name }}',
-
-      {%- if config.get('partitioning_column') %}
-        partition_column => '{{ config.get("partitioning_column") }}',
-      {% endif -%}
-
-      {%- if config.get('number_partitions') %}
-        number_partitions => {{ config.get('number_partitions') }},
-      {% endif -%}
-
-      {%- if config.get('chunk_time_interval') %}
-        chunk_time_interval => {{ config.get('chunk_time_interval') }},
-      {% endif -%}
-
-      {%- if config.get('create_default_indexes') %}
-        create_default_indexes => {{ config.get('create_default_indexes') }},
-      {% endif -%}
-
-      {%- if config.get('partitioning_func') %}
-        partitioning_func => '{{ config.get("partitioning_func") }}',
-      {% endif -%}
-
-      {%- if config.get('associated_schema_name') %}
-        associated_schema_name => '{{ config.get("associated_schema_name") }}',
-      {% endif -%}
-
-      {%- if config.get('associated_table_prefix') %}
-        associated_table_prefix => '{{ config.get("associated_table_prefix") }}',
-      {% endif -%}
-
-      {%- if config.get('time_partitioning_func') %}
-        time_partitioning_func => '{{ config.get("time_partitioning_func") }}',
-      {% endif -%}
-
-      {%- if config.get('replication_factor') %}
-        replication_factor => {{ config.get('replication_factor') }},
-      {% endif -%}
-
-      {%- if config.get('data_nodes') %}
-        data_nodes => '{"{{ config.get("data_nodes")|join("\", \"") }}"}',
-      {% endif -%}
-
-      {%- if config.get('distributed') %}
-        distributed => {{ config.get('distributed') }},
-      {% endif -%}
-
-      if_not_exists => false, {# Users should not be concerned with this #}
-      migrate_data => true); {# Required since dbt models will always contain data #}
+    {{ get_create_hypertable_as_sql(intermediate_relation) }}
 
     {%- if config.get('compression') %}
-      alter table {{ intermediate_relation }} set (timescaledb.compress
-        {%- if config.get("compression").orderby %}
-          ,timescaledb.compress_orderby = '{{ config.get("compression").orderby }}'
-        {% endif -%}
-
-        {%- if config.get("compression").segmentby %}
-          ,timescaledb.compress_segmentby = '{{ config.get("compression").segmentby | join(",") }}'
-        {% endif -%}
-
-        {%- if config.get("compression").chunk_time_interval %}
-          ,timescaledb.compress_chunk_time_interval = '{{ config.get("compression").chunk_time_interval }}'
-        {% endif -%}
-      );
-
-      select add_compression_policy(
-        '{{ intermediate_relation }}',
-        {{ config.get("compression").after }}
-
-        {%- if config.get("compression").schedule_interval %}
-          , schedule_interval => '{{ config.get("compression").schedule_interval }}'
-        {% endif -%}
-
-        {%- if config.get("compression").initial_start %}
-          , initial_start => {{ config.get("compression").initial_start }}
-        {% endif -%}
-
-        {%- if config.get("compression").timezone %}
-          , timezone => '{{ config.get("compression").timezone }}'
-        {% endif -%}
-
-        );
+      {{ enable_compression(intermediate_relation, config.get("compression")) }}
+      {{ add_compression_policy(intermediate_relation, config.get("compression")) }}
     {%- endif -%}
 
   {%- endcall %}
---
+++ dbt/include/timescaledb/macros/relations/hypertable.sql
@@ -0,0 +1,89 @@
+{% macro get_create_hypertable_as_sql(relation) %}
+  select create_hypertable(
+    '{{ relation }}',
+    '{{ config.get("time_column_name") }}',
+
+    {%- if config.get('partitioning_column') %}
+      partition_column => '{{ config.get("partitioning_column") }}',
+    {% endif -%}
+
+    {%- if config.get('number_partitions') %}
+      number_partitions => {{ config.get('number_partitions') }},
+    {% endif -%}
+
+    {%- if config.get('chunk_time_interval') %}
+      chunk_time_interval => {{ config.get('chunk_time_interval') }},
+    {% endif -%}
+
+    {%- if config.get('create_default_indexes') %}
+      create_default_indexes => {{ config.get('create_default_indexes') }},
+    {% endif -%}
+
+    {%- if config.get('partitioning_func') %}
+      partitioning_func => '{{ config.get("partitioning_func") }}',
+    {% endif -%}
+
+    {%- if config.get('associated_schema_name') %}
+      associated_schema_name => '{{ config.get("associated_schema_name") }}',
+    {% endif -%}
+
+    {%- if config.get('associated_table_prefix') %}
+      associated_table_prefix => '{{ config.get("associated_table_prefix") }}',
+    {% endif -%}
+
+    {%- if config.get('time_partitioning_func') %}
+      time_partitioning_func => '{{ config.get("time_partitioning_func") }}',
+    {% endif -%}
+
+    {%- if config.get('replication_factor') %}
+      replication_factor => {{ config.get('replication_factor') }},
+    {% endif -%}
+
+    {%- if config.get('data_nodes') %}
+      data_nodes => '{"{{ config.get("data_nodes")|join("\", \"") }}"}',
+    {% endif -%}
+
+    {%- if config.get('distributed') %}
+      distributed => {{ config.get('distributed') }},
+    {% endif -%}
+
+    if_not_exists => false, {# Users should not be concerned with this #}
+    migrate_data => true {# Required since dbt models will always contain data #}
+  );
+{% endmacro %}
+
+{% macro enable_compression(relation, compression_config) %}
+  alter table {{ relation }} set (timescaledb.compress
+    {%- if compression_config.orderby %}
+        ,timescaledb.compress_orderby = '{{ compression_config.orderby }}'
+    {% endif -%}
+
+    {%- if compression_config.segmentby %}
+        ,timescaledb.compress_segmentby = '{{ compression_config.segmentby | join(",") }}'
+    {% endif -%}
+
+    {%- if compression_config.chunk_time_interval %}
+        ,timescaledb.compress_chunk_time_interval = '{{ compression_config.chunk_time_interval }}'
+    {% endif -%}
+  );
+{% endmacro %}
+
+{% macro add_compression_policy(relation, compression_config) %}
+  select add_compression_policy(
+    '{{ relation }}',
+    {{ compression_config.after }}
+
+    {%- if compression_config.schedule_interval %}
+        , schedule_interval => '{{ compression_config.schedule_interval }}'
+    {% endif -%}
+
+    {%- if compression_config.initial_start %}
+        , initial_start => {{ compression_config.initial_start }}
+    {% endif -%}
+
+    {%- if compression_config.timezone %}
+        , timezone => '{{ compression_config.timezone }}'
+    {% endif -%}
+
+  );
+{% endmacro %}
