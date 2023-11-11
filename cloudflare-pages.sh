1ca8228e4122017f78c82ee2bf00be114bcd73e0
Sam Debruyn <sam@debruyn.dev>
2023-11-12 00:35:34 +0100

    install wheel in CF

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -3,6 +3,8 @@
 set -ex
 
 python -V
+python -m pip install --upgrade pip
+python -m pip install --upgrade --user wheel
 python -m pip install --user pdm
 python -m pdm sync
 python -m pdm run mkdocs build
