e9f32a568b0f572c069c647482ed6407422d7308
Sam Debruyn <sam@debruyn.dev>
2023-11-12 00:45:03 +0100

    fail fast and no editable installs

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -6,5 +6,5 @@ python -V
 python -m pip install --upgrade pip
 python -m pip install --upgrade --user wheel
 python -m pip install --user pdm
-python -m pdm install -dG docs
+python -m pdm install -x --no-editable -dG docs
 python -m pdm run mkdocs build
