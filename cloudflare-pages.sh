6e3f448751cb2d65618d350068c217b58148d417
Sam Debruyn <sam@debruyn.dev>
2024-03-27 21:37:29 +0100

    echo and do not install default deps

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
+echo "Build started on `date`"
+
 python -V
 python -m pip install --upgrade pip
 python -m pip install --upgrade --user wheel
 python -m pip install --user pdm
-python -m pdm install -x --no-editable -dG docs
+python -m pdm install -x --no-default --no-editable -G docs
 python -m pdm run mkdocs build
