20b3b10ad738bea24d761e94fb04689e97ccc9db
Sam Debruyn <sam@debruyn.dev>
2024-03-27 21:52:11 +0100

    faster builds

--- docs_build/cloudflare-pages.sh
+++ docs_build/cloudflare-pages.sh
@@ -2,12 +2,6 @@
 
 set -ex
 
-echo "Build started on `date`"
 cd ..
-
-python -V
-python -m pip install --upgrade pip
-python -m pip install --upgrade --user wheel
-python -m pip install --user pdm
 python -m pdm install -x --no-default --no-editable -G docs
-python -m pdm run mkdocs build
+python -m pdm run mkdocs build -d ./docs_build/site
