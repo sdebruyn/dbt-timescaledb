4ea6535cc805b61c33e4323e3d5c537fdb552329
Sam Debruyn <sam@debruyn.dev>
2025-10-18 22:03:59 +0200

    remove swetrix

--- docs_build/cloudflare-pages.sh
+++ docs_build/cloudflare-pages.sh
@@ -6,6 +6,4 @@ cd ..
 python -m pdm install -x --no-default --no-editable -G docs
 python -m pdm run mkdocs build -d ./docs_build/site
 
-curl -sLo ./docs_build/site/t1.js "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js"
-curl -sLo ./docs_build/site/t1.js.map "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js.map"
-curl -sLo ./docs_build/site/t2.js "https://umami.debruyn.dev/script.js"
+curl -sLo ./docs_build/site/t.js "https://umami.debruyn.dev/script.js"
